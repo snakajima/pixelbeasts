fad4f63c969b11a9825f96571c7bb536d608829b
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2021-10-09 06:19:31 -1000

    About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
-    <h1>To be filled</h1>
+    <h1 class="text-4xl font-bold my-6">PixelBeasts</h1>
+    <p class="mx-16">PixelBeasts are 10,000 unique NFT collectibles, 24x24 pixels each by artist and VC, Yohei Nakajima.
+      Ownership provides access to Beastopia - an ever evolving social experiences including events, games,
+      and more - alongside a community of VCs, founders, devs, and NFT enthusiasts 
+      venturing deeper down this rabbit hole together, hand-in-hand, unafraid and optimistic.</p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,13 +4,13 @@
       <!-- <img alt="Firebase logo" src="../assets/firebase.svg" class="logo" /> -->
       <!-- Use Tailwind CSS h-40 (=10rem=160px) instead of .logo. -->
       <img
-        alt="Firebase logo"
-        src="../assets/firebase.svg"
-        class="h-40 object-contain"
+        alt="Beastopia"
+        src="../assets/field.jpeg"
+        class="h-80 object-contain"
       />
     </div>
 
-    <h1 class="text-4xl font-bold my-6">Welcome to Pixel Beasts</h1>
+    <h1 class="text-4xl font-bold my-6">Welcome to PixelBeasts (Beastopia)</h1>
   </div>
 </template>
 
