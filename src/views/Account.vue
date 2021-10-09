b51a1ab4ce1e52bd25bfd9082a8c14f65414e78d
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2021-10-08 20:23:02 -1000

    hasMetaMask

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,30 +1,33 @@
 <template>
   <div class="account">
-    <a
-      @click="metaMaskSignin"
-      class="bg-black bg-opacity-5 shadow-lg inline-flex justify-center items-center h-12 px-6 rounded-lg hover:bg-green-600 hover:text-white"
-    >
-      Signin with MetaMask
-    </a>
+    <div v-if="hasMetaMask">
+      <a
+        @click="metaMaskSignin"
+        class="bg-black bg-opacity-5 shadow-lg inline-flex justify-center items-center h-12 px-6 rounded-lg hover:bg-green-600 hover:text-white"
+      >
+        Signin with MetaMask
+      </a>
+    </div>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, computed } from "vue";
 import { ethereum } from "../utils/MetaMask";
+import { mapState } from 'vuex';
 
 export default defineComponent({
   name: "Account",
   setup() {
+    const hasMetaMask = (typeof ethereum !== 'undefined');
+    const metaMaskSignin = async () => {
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      const account = accounts[0];
+      // this.store.account = account;
+      alert("account:"+account);
+    };
     return {
-        metaMaskSignin: async () => {
-          if (typeof ethereum !== 'undefined') {
-            const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-            const account = accounts[0];
-            alert(account);
-          } else {
-            alert("MetMask is NOT installed")
-          }
-        },
+      hasMetaMask,
+      metaMaskSignin
     }
   }
 });
