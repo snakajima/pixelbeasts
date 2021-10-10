c56ae349c15eff366f96c89d74ef04a59fbffd3c
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2021-10-10 10:35:25 -1000

    custom token

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,10 @@
 import * as functions from "firebase-functions";
 import * as util from "ethereumjs-util";
+import * as admin from "firebase-admin";
+
+if (!admin.apps.length) {
+  admin.initializeApp();
+}
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -16,7 +21,7 @@ export const generateNonce = functions.https.onCall((data, context) => {
   return {nonce: message};
 });
 
-export const verifyNonce = functions.https.onCall((data, context) => {
+export const verifyNonce = functions.https.onCall(async (data, context) => {
   const signature = data.signature;
   const account = data.account;
   const nonce = "\x19Ethereum Signed Message:\n" + message.length + message;
@@ -25,5 +30,11 @@ export const verifyNonce = functions.https.onCall((data, context) => {
   const pubKey = util.ecrecover(util.toBuffer(nonceBuffer), v, r, s);
   const addrBuf = util.pubToAddress(pubKey);
   const addr = util.bufferToHex(addrBuf);
-  return {account, addr};
+  if (account == addr) {
+    const auth = admin.auth();
+    const token = await auth.createCustomToken(account);
+    return {token};
+  } else {
+    return {"error": "mismatch"};
+  }
 });
