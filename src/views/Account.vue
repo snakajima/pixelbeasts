d86e9eecc2246d62419f8f1c5a904aba1a3f6549
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2021-10-09 19:34:14 -1000

    personal_sign

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="account">
     <div v-if="hasMetaMask">
-      <div v-if="account">{{ account }}</div>
+      <div v-if="account">
+        <p>{{ account }}</p>
+        <a 
+          @click="test"
+          class="bg-black bg-opacity-5 shadow-lg inline-flex justify-center items-center h-12 px-6 rounded-lg hover:bg-green-600 hover:text-white"
+        >
+          Test
+        </a>
+      </div>
       <a v-else
         @click="metaMaskSignin"
         class="bg-black bg-opacity-5 shadow-lg inline-flex justify-center items-center h-12 px-6 rounded-lg hover:bg-green-600 hover:text-white"
@@ -17,7 +25,7 @@
 <script lang="ts">
 import { defineComponent, computed } from "vue";
 import { useStore } from "vuex";
-import { hasMetaMask, requestAccount, getAccount } from "../utils/MetaMask";
+import { hasMetaMask, requestAccount, ethereum } from "../utils/MetaMask";
 
 export default defineComponent({
   name: "Account",
@@ -26,8 +34,16 @@ export default defineComponent({
     const metaMaskSignin = async () => {
       await requestAccount(); // ethereum.on('accountsChanged') will handle the result
     };
+    const test = async () => {
+      const account = store.state.account;
+      const message = "Hello World";
+      const result = await ethereum.request({ method: 'personal_sign', params: [message, account] });
+      //const result = await ethereum.request({ method: 'eth_accounts' });
+      alert(result);
+    }
     const account = computed(() => store.state.account);
     return {
+      test,
       account,
       hasMetaMask,
       metaMaskSignin
