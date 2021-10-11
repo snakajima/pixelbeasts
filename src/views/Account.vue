96f4ce7643de03ff3d5a79c9d13fad19c1ced78f
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2021-10-10 18:12:24 -1000

    query params

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -76,7 +76,16 @@ export default defineComponent({
       await auth.signOut();
     };
     const fetchAssets = async () => {
-      const url = "https://api.opensea.io/api/v1/assets?owner=0xf05a0497994a33f18aa378630bc674efc77ad557&order_direction=desc&offset=0&limit=20&collection=beastopia-pixelbeasts";
+      const account = store.state.account;
+      const params: Record<string, string> = {
+        "owner": String(account),
+        "order_direction": "desc",
+        "offset": String(0),
+        "limit": String(40),
+        "collection": "beastopia-pixelbeasts"
+      };
+      const query = Object.keys(params).map(key => { return key+"="+encodeURIComponent(params[key])});
+      const url = "https://api.opensea.io/api/v1/assets?"+query.join('&');
       const result = await fetch(url);
       const json = await result.json();
       alert(JSON.stringify(json));
