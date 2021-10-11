003e4a49c571105d0bb0c4b1bcf7836905b60831
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2021-10-11 12:11:24 -1000

    passing uuid

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,6 +5,7 @@ import * as admin from "firebase-admin";
 if (!admin.apps.length) {
   admin.initializeApp();
 }
+// const db = admin.firestore();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -14,16 +15,18 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
 
-const message = "PixelBeasts needs to verify your identity." +
+const readableMessage = "PixelBeasts needs to verify your identity." +
                 "Please sign this message.";
 
 export const generateNonce = functions.https.onCall((data, context) => {
-  return {nonce: message};
+  const uuid = "#abcdefg";
+  return {nonce: readableMessage + uuid, uuid};
 });
 
 export const verifyNonce = functions.https.onCall(async (data, context) => {
   const signature = data.signature;
   const account = data.account;
+  const message = data.nonce; // debug only
   const nonce = "\x19Ethereum Signed Message:\n" + message.length + message;
   const nonceBuffer = util.keccak(Buffer.from(nonce, "utf-8"));
   const {v, r, s} = util.fromRpcSig(signature);
--- src/views/Account.vue
+++ src/views/Account.vue
@@ -71,10 +71,13 @@ export default defineComponent({
       const generateNonce = functions.httpsCallable('generateNonce');
       const result = await generateNonce({account});
       const nonce = result.data.nonce;
+      const uuid = result.data.uuid;
+      console.log("verifyIdentity: uuid/nonce", uuid, nonce);
       const signature = await ethereum.request({ method: 'personal_sign', params: [nonce, account] });
       const verifyNonce = functions.httpsCallable('verifyNonce');
-      const result2 = await verifyNonce({account, signature});
+      const result2 = await verifyNonce({account, signature, uuid, nonce}); // Note: this nonce is debugging
       const token = result2.data.token; 
+      console.log("verifyIdentity: token", token)
       const credential = await auth.signInWithCustomToken(token);
       const user = credential.user; 
     };
