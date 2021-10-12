3392767c63c1347dad63109d2a3d882d46e3bb86
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2021-10-11 20:49:22 -1000

    opensea.ts

---
+++ functions/src/opensea.ts
@@ -0,0 +1,19 @@
+import {request} from "./lib/netutils";
+
+export const fetchAsset = async (account: string, tokenId:string,
+    collection: string) => {
+  const params: Record<string, string> =
+    {"owner": String(account),
+      "order_direction": "desc",
+      "offset": String(0),
+      "limit": String(40),
+      collection,
+      tokenId};
+  const query = Object.keys(params).map((key) => {
+    return key+"="+encodeURIComponent(params[key]);
+  });
+  const url = "https://api.opensea.io/api/v1/assets?"+query.join("&");
+  const json = await request(url, {});
+  const assets = json["assets"];
+  return assets.length > 0 ? assets[0] : null;
+};
