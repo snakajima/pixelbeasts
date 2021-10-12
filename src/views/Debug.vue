8df60fd5a1ff97d988609cf005920dc4f959449c
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2021-10-11 16:30:06 -1000

    defineComponent for Debug page

--- src/views/Debug.vue
+++ src/views/Debug.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
-    Debug
+    Debug {{ account }}
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, computed } from "vue";
+import { useStore } from "vuex";
+import { hasMetaMask, requestAccount, ethereum } from "../utils/MetaMask";
+import { functions, auth } from "../utils/firebase";
+
+export default defineComponent({
+  name: "Account",
+  setup() {
+    const store = useStore();
+    const account = computed(() => store.state.account);
+    const assets = computed(() => store.state.assets);
+    return {
+      assets,
+      account,
+    }
+  }
+});
+</script>
