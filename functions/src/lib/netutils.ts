aadbe08132f003ee42af35424dbdea5e4668f9f1
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2021-10-11 20:47:48 -1000

    netutils from ownplate

---
+++ functions/src/lib/netutils.ts
@@ -0,0 +1,36 @@
+import * as https from "https";
+import * as url from "url";
+
+export const request = (_url: string, _options: any, postData?: any):
+    Promise<any> => {
+  const parsedURL = url.parse(_url);
+  const options = Object.assign(
+      {
+        hostname: parsedURL.host,
+        port: 443,
+        path: parsedURL.pathname,
+        method: "GET",
+      },
+      _options
+  );
+  return new Promise((resolve, reject) => {
+    const req = https.request(options, (res) => {
+      res.setEncoding("utf8");
+      let body = "";
+      res.on("data", (chunk) => {
+        body += chunk;
+      });
+      res.on("end", () => {
+        resolve(JSON.parse(body));
+      });
+    });
+    req.on("error", (e) => {
+      console.error(e);
+      reject(e);
+    });
+    if (postData) {
+      req.write(postData);
+    }
+    req.end();
+  });
+};
