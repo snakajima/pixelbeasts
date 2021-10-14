f6643e01c7a6e5ff99808b2da9eb96a8510b6828
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2021-10-13 14:05:19 -1000

    isCreating

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,32 +1,38 @@
 <template>
   <div class="about">
-    <h1 class="text-4xl font-bold my-6">PixelBeasts</h1>
+    <h1 class="text-4xl font-bold my-6">{{ asset.token_id }}</h1>
+    <div v-if="isCreating">
+        Creating...
+        <a @click="() => {newRoom(false)}"
+    class="m-2 bg-black bg-opacity-5 shadow-lg inline-flex justify-center items-center px-6 rounded-lg hover:bg-green-600 hover:text-white">
+        <span>Cancel</span>
+        </a>
+    </div>
+    <a v-else @click="() => {newRoom(true)}"
+class="m-2 bg-black bg-opacity-5 shadow-lg inline-flex justify-center items-center px-6 rounded-lg hover:bg-green-600 hover:text-white">
+    <span>New Room</span>
+    </a>
+
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from "vue";
+import { defineComponent, computed, ref } from "vue";
 import { useStore } from "vuex";
-import { hasMetaMask, requestAccount, ethereum } from "../utils/MetaMask";
-import { functions, auth } from "../utils/firebase";
 
 export default defineComponent({
   name: "Account",
   setup() {
     const store = useStore();
-    const account = computed(() => store.state.account);
     const asset = computed(() => store.getters.asset);
-    const debug1 = async () => {
-      try {
-        const debug1 = functions.httpsCallable('debug1');
-        const result = await debug1({account, tokenId:asset.value.token_id});
-        alert("success!");
-      } catch(e) {
-        alert(e);
-      }
+    const isCreating = ref(false);
+    const newRoom = (flag: boolean) => {
+      isCreating.value = flag;
     };
     return {
-      debug1,
+      isCreating,
+      asset,
+      newRoom,
     }
   }
 });
