bf5bd0196bf3d0d2be879cc38a0189d27ee5b2de
Isamu Arimoto <isamu@to-kyo.to>
2021-10-16 04:35:13 +0900

    fix build

--- src/utils/OpenSea.ts
+++ src/utils/OpenSea.ts
@@ -4,8 +4,8 @@ export const fetchAssets = async (account: string, collection: string) => {
     offset: String(0),
     limit: String(40),
     collection: collection,
+    order_direction: "desc", // eslint-disable-line @typescript-eslint/camelcase
   };
-  params["order_direction"] = "desc"; // work around strict lint
   const query = Object.keys(params).map((key) => {
     return key + "=" + encodeURIComponent(params[key]);
   });
