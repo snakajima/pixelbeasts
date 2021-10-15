837220a13dddf5bbc1055e222a1436c1005f011b
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2021-10-15 04:54:43 -1000

    fix build

--- src/utils/OpenSea.ts
+++ src/utils/OpenSea.ts
@@ -1,11 +1,11 @@
 export const fetchAssets = async (account: string, collection: string) => {
   const params: Record<string, string> = {
     owner: String(account),
-    order_direction: "desc",
     offset: String(0),
     limit: String(40),
     collection: collection,
   };
+  params["order_direction"] = "desc"; // work around strict lint
   const query = Object.keys(params).map((key) => {
     return key + "=" + encodeURIComponent(params[key]);
   });
